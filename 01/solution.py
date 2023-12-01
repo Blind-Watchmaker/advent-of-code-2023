b6e5f29a152cb4ee8c51a1ed9fa5f2c5bcb41c0c
Will Musgrave <will@bluenode.ca>
2023-12-01 11:52:03 -0400

    Add solution to puzzle 1

---
+++ 01/solution.py
@@ -0,0 +1,28 @@
+import re
+
+file_path = 'input.txt'
+
+def extract_digit(line):
+    match = re.search(r'\d', line)
+    if match:
+        return match.group()
+    else:
+        raise Exception("No digit found in the line!")
+
+# Open the file and go through it line by line
+sum = 0
+with open(file_path, 'r') as file:
+    for line in file:
+        # Strip leading and trailing whitespace
+        line = line.strip()
+        first_digit = extract_digit(line)
+        last_digit = extract_digit(line[::-1])
+        calibration_value = int(first_digit + last_digit)
+        sum += calibration_value
+        print(f"Input string: {line}")
+        print(f"First digit: {first_digit}")
+        print(f"Last digit: {last_digit}")
+        print(f"Calibration value: {calibration_value}")
+        print("")
+
+print(f"Sum: {sum}")
